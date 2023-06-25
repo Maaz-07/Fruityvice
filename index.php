88929498840da0f1b50523e7b40ea8e894df9bfe
Maaz-07 <maazchoudhry07@gmail.com>
2023-06-25 10:26:43 +0500

    Save Data in DB

--- index.php
+++ index.php
@@ -16,6 +16,16 @@ curl_close($ch);
 
 $data = json_decode($response, true);
 if ($data) {
+    $hostname = 'localhost';
+    $database = 'fruityvice';
+    $username = 'root';
+    $password = '';
+    $connection = mysqli_connect($hostname, $username, $password, $database);
+
+    if (mysqli_connect_errno()) {
+        die("Failed to connect to MySQL: " . mysqli_connect_error());
+    }
+
     foreach ($data as $fruit) {
         echo "Name: " . $fruit['name'] . "<br>";
         echo "ID: " . $fruit['id'] . "<br>";
@@ -32,7 +42,17 @@ if ($data) {
             echo "Protein: " . $nutritions['protein'] . "<br>";
         }
         echo "<br>";
+
+        $sql = "INSERT INTO fruits (id, name, family, genus, order_name, nutritions) 
+                VALUES (" . $fruit['id'] . ", '" . mysqli_real_escape_string($connection, $fruit['name']) . "', '" . mysqli_real_escape_string($connection, $fruit['family']) . "', '" . mysqli_real_escape_string($connection, $fruit['genus']) . "', '" . mysqli_real_escape_string($connection, $fruit['order']) . "', '" . mysqli_real_escape_string($connection, json_encode($fruit['nutritions'])) . "')";
+
+        if (mysqli_query($connection, $sql)) {
+            echo "Fruit saved to the database successfully.<br>";
+        } else {
+            echo "Error: " . mysqli_error($connection);
+        }
     }
+    mysqli_close($connection);
 } else {
     echo "No fruit data found.";
 }
